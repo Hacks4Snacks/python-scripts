f3c5b4cb5d12eae1afea10843317aee712dd5de1
hacks4snacks <markdaltongray@gmail.com>
2020-04-28 18:05:37 -0400

    added additional comments

--- tcp_client.py
+++ tcp_client.py
@@ -1,11 +1,12 @@
 #!/bin/env python3
+# A simple TCP client script that establishes a socket, connected to target_host on target_port, sends message, and gets a response
 
 import socket
 
 target_host = "google.com"
 target_port = 80
 
-# create socket object
+# create socket object, AF_INET = IPv4, SOCK_STREAM = TCP
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect client
