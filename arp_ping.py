ab10afeb2970618cda240a33efef3aec9329ce15
hacks4snacks <markdaltongray@gmail.com>
2020-05-06 13:56:23 -0400

    added valid IP/CIDR and user check. Also created functions for logic

--- arp_ping.py
+++ arp_ping.py
@@ -3,17 +3,42 @@
 
 from scapy.all import *
 import sys
+import os
+import ipaddress
 
-if len(sys.argv) != 2:
-    print(f'Usage: arp_ping.py <network>')
-    print(f'Example: ./arp_ping.py 192.168.1.1/24')
-    sys.exit(1)
+def prerun_test():
 
-conf.verb = 0
+    if not os.geteuid() == 0:
+        sys.exit('This script must be run as root.')
 
-ans, unans = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=sys.argv[1]), timeout=2)
+    if len(sys.argv) != 2:
+        print(f'Usage: arp_ping.py <network>')
+        print(f'Example: ./arp_ping.py 192.168.1.0/24')
+        sys.exit(1)
 
-print("MAC ADDRESS       IP ADDRESS")
+    try:
+        ip_network = ipaddress.ip_network(sys.argv[1]) # is argv a valid IP or IP/CIDR combination
+    
+    except ValueError:
+        print(f'{sys.argv[1]} is an invalid IP network')
+        print(f'Usage: arp_ping.py <network>')
+        sys.exit(f'Example: ./arp_ping.py 192.168.1.0/24')
 
-for s,r in ans:
-    print(r.sprintf("%Ether.src% %ARP.psrc%"))
+
+def arp_ping(network):
+
+    conf.verb = 0
+
+    ans, unans = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=sys.argv[1]), timeout=2)
+
+    print("MAC ADDRESS       IP ADDRESS")
+
+    for s,r in ans:
+        print(r.sprintf("%Ether.src% %ARP.psrc%"))
+
+def main():
+    prerun_test()
+    arp_ping(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
